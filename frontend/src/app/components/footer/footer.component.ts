196e54e86ce30ab0b4b581a9e247153208dd948d
Robson Xavier <rbsmail93@gmail.com>
2023-10-11 16:42:33 -0300

    create footer component

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import Constants from 'src/app/utils/constants';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-
+  githubUrl = Constants.GITHUB_URL;
 }
