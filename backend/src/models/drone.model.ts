1627f0726918828b53520edeb13c37b5b968ab52
Robson Xavier <rbsmail93@gmail.com>
2023-10-11 12:53:16 -0300

    add class-validator package; add validations on CalculateRouteDto class

--- backend/src/models/drone.model.ts
+++ backend/src/models/drone.model.ts
@@ -1,10 +1,26 @@
-export type CalculateRouteDto = {
-  startPoint: string;
-  objectPoint: string;
-  destinationPoint: string;
-};
+import { Length, Matches } from 'class-validator';
 
-export type CalculateRouteResultDto = {
+export class CalculateRouteDto {
+  @Matches('[A-H][1-8]')
+  @Length(2, 2, {
+    message: 'startPoint must be equal to 2 characters',
+  })
+  startPoint: string;
+
+  @Matches('[A-H][1-8]')
+  @Length(2, 2, {
+    message: 'objectPoint must be equal to 2 characters',
+  })
+  objectPoint: string;
+
+  @Matches('[A-H][1-8]')
+  @Length(2, 2, {
+    message: 'destinationPoint must be equal to 2 characters',
+  })
+  destinationPoint: string;
+}
+
+export class CalculateRouteResultDto {
   path: string;
   totalSeconds: number;
-};
+}
